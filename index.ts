63e536e9310959b0f1a5d22c2a1ee5f36dd9c336
Sozan <suzan.ayesh11@gmail.com>
2023-10-23 18:13:15 +0300

    actions 20

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import AWS from 'aws-sdk';
 import express from 'express';
 import logger from 'morgan';
 import dataSource from './db/dataSource';
@@ -10,34 +11,102 @@ app.use(express.json());
 app.use(logger('dev'));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
-// Define your POST route for signup
-app.post('/signup', (req, res) => {
+
+const s3 = new AWS.S3({
+  region: 'us-east-1',
+  accessKeyId: process.env.ACCESSKEYID,
+  secretAccessKey: process.env.SECRETACCESSKEY,
+  signatureVersion: 'v4'
+});
+app.post('/category', (req, res) => {
+  let fileType = req.body.type; 
+  let uploadData;
+  let keyPath = '';
   
-  res.status(200).json({ message: 'Signup successful' });
+  switch (fileType) {
+    case 'link':
+      uploadData = Buffer.from(req.body.link);
+      keyPath = 'links/' + Date.now() + '.txt';
+      break;
+    case 'json':
+      uploadData = Buffer.from(JSON.stringify(req.body.data));
+      keyPath = 'json/' + Date.now() + '.json';
+      break;
+    case 'text':
+      uploadData = Buffer.from(req.body.text);
+      keyPath = 'text/' + Date.now() + '.txt';
+      break;
+    default:
+      return res.status(400).send("Invalid file type");
+  }
+
+  s3.putObject({
+    Bucket: 'suzans3',
+    Key: keyPath,
+    Body: uploadData
+  }, (error: any, data: any) => {
+    if (error) {
+      console.error(`Error uploading ${fileType} to S3:`, error);
+      res.status(500).send("Error uploading to S3");
+    } else {
+      res.status(200).json({ message: `${fileType.charAt(0).toUpperCase() + fileType.slice(1)} uploaded successfully` });
+    }
+  });
 });
 
+
+// // Upload link
+// app.post('/category', (req, res) => {
+//   const link = req.body.link;
+//   s3.putObject({
+//     Bucket: 'suzans3',
+//     Key: 'links/' + Date.now() + '.txt',
+//     Body: Buffer.from(link)
+//   }, (error: any, data: any) => {
+//     if (error) {
+//       console.error("Error uploading link to S3:", error);
+//       res.status(500).send("Error uploading to S3");
+//     } else {
+//       res.status(200).json({ message: "Link uploaded successfully" });
+//     }
+//   });
+// });
+
+// // Upload JSON
+// app.post('/category', (req, res) => {
+//   const jsonData = JSON.stringify(req.body);
+//   s3.putObject({
+//     Bucket: 'suzans3',
+//     Key: 'json/' + Date.now() + '.json',
+//     Body: Buffer.from(jsonData)
+//   }, (error: any, data: any) => {
+//     if (error) {
+//       console.error("Error uploading JSON to S3:", error);
+//       res.status(500).send("Error uploading to S3");
+//     } else {
+//       res.status(200).json({ message: "JSON uploaded successfully" });
+//     }
+//   });
+// });
+
+// // Upload text
+// app.post('/category', (req, res) => {
+//   const text = req.body.text;
+//   s3.putObject({
+//     Bucket: 'suzans3',
+//     Key: 'text/' + Date.now() + '.txt',
+//     Body: Buffer.from(text)
+//   }, (error: any, data: any) => {
+//     if (error) {
+//       console.error("Error uploading text to S3:", error);
+//       res.status(500).send("Error uploading to S3");
+//     } else {
+//       res.status(200).json({ message: "Text uploaded successfully" });
+//     }
+//   });
+// });
+
 const PORT = process.env.PORT || 5000;
-
-// const AWS = require('aws-sdk');
-
-// let s3 = new AWS.S3({
-//   region: 'us-east-1', // Uncomment and adjust as needed
-//   accessKeyId: process.env.ACCESSKEYID,
-//   secretAccessKey: process.env.SECRETACCESSKEY
-// });
-
-// s3.putObject({
-//   Bucket: 'suzans3',
-//   Key: 'my-txt-file.txt',
-//   Body: Buffer.from('This is my text file')
-// }, (error, data) => {
-//   if (error) {
-//     console.error("Error uploading to S3:", error);
-//   } else {
-//     console.log("Successfully uploaded to S3:", data.ETag);
-//   }
-// });
-
 
 dataSource.initialize().then(() => {
   console.log('Connected to DB!');
